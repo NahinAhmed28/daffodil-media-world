53ab044991d57829039845a33dfd6ae00dcec310
Nahin <nahin.ahmed28@gmail.com>
2023-02-21 23:07:03 +0600

    product module completed

--- resources/views/frontend/layouts/product.blade.php
+++ resources/views/frontend/layouts/product.blade.php
@@ -2,88 +2,53 @@
 
 @section('content')
 
-@push('style')
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
-@endpush
+    @push('style')
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    @endpush
 
-<!-- ======= Hero Section ======= -->
-@include('frontend.layouts.common.heroheader')
-<!-- End Hero -->
+    <!-- ======= Hero Section ======= -->
+    @include('frontend.layouts.common.heroheader')
+    <!-- End Hero -->
 
-<!-- Start #main -->
-<main id="main">
+    <!-- Start #main -->
+    <main id="main">
 
-
-
-    <!-- ======= Team Section ======= -->
-    <section id="team" class="team section-bg">
-        <div class="container" data-aos="fade-up">
-
-            <div class="section-title">
-                <h2>Team</h2>
-                <p>Check our Team</p>
-            </div>
-
-            <div class="row">
-
-                @foreach ($products as $product)
-                <div class="col-lg-4 col-md-6">
-                    <div class="product" data-aos="fade-up" data-aos-delay="100">
-                        <div class="pic"><img src="{{ asset('assets/uploads/product/'. $product->image) }}"
-                                class="img-fluid" alt=""></div>
-                        <div class="product-info">
-                            <h4>{{$product->name}}</h4>
-                            <span>{{$product->designation}}</span>
-                            <div class="social">
-                                <a href=""><i class="bi bi-twitter"></i></a>
-                                <a href=""><i class="bi bi-facebook"></i></a>
-                                <a href=""><i class="bi bi-instagram"></i></a>
-                                <a href=""><i class="bi bi-linkedin"></i></a>
+        <!-- ======= About Boxes Section ======= -->
+        <section id="about-boxes" class="about-boxes">
+            <div class="container" data-aos="fade-up">
+                <div class="section-title">
+                    <h2>Our</h2>
+                    <p>Services</p>
+                </div>
+                <div class="row">
+                    @foreach ($products as $product)
+                        <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+                            <div class="card">
+                                <img src="{{ asset('assets/uploads/product/'.$product->image)}}" class="card-img-top"
+                                     alt="...">
+                                <div class="card-icon">
+                                    <i class="ri-brush-4-line"></i>
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title"><a href="">{!!$product->title!!}</a></h5>
+                                    <p class="card-text">{!!$product->description!!}</p>
+                                    <p class="card-text"><strong>Model:</strong>  {!!$product->model!!}</p>
+                                    <p class="card-text"><strong>Category:</strong> {!!$product->category!!}</p>
+                                    <p class="card-text"><strong>Stock:</strong> {!!$product->stock!!}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                @endforeach
-            </div>
-
-        </div>
-    </section><!-- End Team Section -->
-
-
-    <!-- ======= Testimonials Section ======= -->
-    <section id="testimonials" class="testimonials">
-        <div class="container" data-aos="zoom-in">
-
-            <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
-                <div class="swiper-wrapper">
-                    @foreach ($products as $product)
-                    <div class="swiper-slide">
-                        <div class="testimonial-item">
-                            <img src="{{ asset('assets/uploads/product/'. $product->image) }}" class="testimonial-img"
-                                alt="">
-                            <h3>{{$product->name}}</h3>
-                            <h4>{{$product->description}}</h4>
-                            <p>
-                                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                                {{$product->model}}
-                                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                            </p>
-                        </div>
-                    </div><!-- End testimonial item -->
                     @endforeach
                 </div>
-                <div class="swiper-pagination"></div>
+
             </div>
+        </section><!-- End About Boxes Section -->
+    </main><!-- End #main -->
 
-        </div>
-    </section><!-- End Testimonials Section -->
-
-</main><!-- End #main -->
-
-@push('scripts')
-<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
-<script>
-    flatpickr("input[type=datetime-local]");
-</script>
-@endpush
+    @push('scripts')
+        <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+        <script>
+            flatpickr("input[type=datetime-local]");
+        </script>
+    @endpush
 @endsection
