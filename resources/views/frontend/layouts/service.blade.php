90db9eaffa6f3bd297e350beaa8bba1d20c5b7d3
Nahin <nahin.ahmed28@gmail.com>
2023-02-25 01:44:15 +0600

    str limit added in service page

--- resources/views/frontend/layouts/service.blade.php
+++ resources/views/frontend/layouts/service.blade.php
@@ -22,29 +22,15 @@
                 </div>
                 <div class="row">
                     @foreach ($services as $service)
-{{--                        <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">--}}
-{{--                            <div class="card">--}}
-{{--                                <img src="{{ asset('assets/uploads/service/'.$service->image)}}" class="card-img-top"--}}
-{{--                                     alt="...">--}}
-{{--                                <div class="card-icon">--}}
-{{--                                    <i class="ri-brush-4-line"></i>--}}
-{{--                                </div>--}}
-{{--                                <div class="card-body">--}}
-{{--                                    <h5 class="card-title"><a href="">{!!$service->title!!}</a></h5>--}}
-{{--                                    <p class="card-text">{!!$service->description!!}</p>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-
 
                         <div class="col-lg-4 col-md-6 portfolio-item filter-app">
                             <a href="{{ asset('assets/uploads/service/'.$service->image) }}"
                                data-gallery="serviceGallery" class="portfolio-lightbox preview-link "
-                               title="{{$service->title}}">
+                               title="{{$service->description}}">
                                 <img src="{{ asset('assets/uploads/service/'.$service->image)}}" class="img-fluid" alt="">
                                 <div class="portfolio-info">
                                     <h4 class="text-center font-weight-bold">{{$service->title}}</h4>
-                                        <p class="card-text px-2">{!!$service->description!!}</p>
+                                        <p class="card-text px-2">{!! Str::limit(strip_tags($service->description), 100) !!} ...</p>
                                 </div>
                             </a>
                         </div>
