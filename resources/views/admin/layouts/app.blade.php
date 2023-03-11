c840c1b9c1a2197ee419255c83abf5e1e907aa17
Nahin <nahin.ahmed28@gmail.com>
2023-03-11 16:06:49 +0600

    product search on admin added

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
+@stack('styles')
 <head>
 
     @include('admin.layouts.common.head')
--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -16,57 +16,86 @@
             </a>
         </div>
 
+                <div class="row">
+                    <div class="col-md-12">
+                        <input id="myInput" type="text" placeholder="Search.." class="text-end">
+                        <br><br>
+                        <div class="table table-responsive">
 
-        <table class="table table-hover table-striped">
-            <thead class="badge-light">
-                <th>ID</th>
-                <th>Image</th>
-                <th>Title</th>
-                <th>Description</th>
-                <th>Model</th>
-                <th>Stock</th>
-                <th>status</th>
-                <th>Action </th>
-
-            </thead>
-            <tbody>
-                @foreach($products as $product)
+                            <table id="products" class="table table-bordered">
+                <thead>
                 <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>
-                        <img class="img-thumbnail image-height"
-                            src="{{ asset('assets/uploads/product/'.$product->image)}}">
-                    </td>
-                    <td>{{$product->title}}</td>
-                    <td>{!!$product->description!!}</td>
-                    <td>{!!$product->model!!}</td>
-                    <td>{!!$product->stock!!}</td>
-                    <td>{!!$product->status!!}</td>
-                    <td>
-                        <a href="{{ route('product.edit',[$product->id]) }}" title="Edit">
-                            <button class="btn btn-outline-primary btn-sm"><i class="fa fa-edit" aria-hidden="true"></i>
-                            </button></a>
-                        <form method="POST" action="{{ route('product.destroy' ,  [$product->id]) }}"
-                            accept-charset="UTF-8" style="display:inline">
-                            @method('DELETE')
-                            @csrf
-                            <button type="submit" class="btn btn-danger btn-sm show-alert-delete-box"
-                                title="Delete Research" {{-- onclick="return confirm(&quot;Confirm delete?&quot;)"
-                                --}}><i class="fa fa-trash" aria-hidden="true"></i></button>
-                        </form>
-                    </td>
+                    <th>ID</th>
+                    <th>Image</th>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Model</th>
+                    <th>Stock</th>
+                    <th>status</th>
+                    <th>Action </th>
                 </tr>
+                </thead>
+                <tbody>
+                @foreach($products as $product)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>
+                            <img class="img-thumbnail image-height"
+                                 src="{{ asset('assets/uploads/product/'.$product->image)}}">
+                        </td>
+                        <td>{{$product->title}}</td>
+                        <td>{!!$product->description!!}</td>
+                        <td>{!!$product->model!!}</td>
+                        <td>{!!$product->stock!!}</td>
+                        <td>{!!$product->status!!}</td>
+                        <td>
+                            <a href="{{ route('product.edit',[$product->id]) }}" title="Edit">
+                                <button class="btn btn-outline-primary btn-sm"><i class="fa fa-edit" aria-hidden="true"></i>
+                                </button></a>
+                            <form method="POST" action="{{ route('product.destroy' ,  [$product->id]) }}"
+                                  accept-charset="UTF-8" style="display:inline">
+                                @method('DELETE')
+                                @csrf
+                                <button type="submit" class="btn btn-danger btn-sm show-alert-delete-box"
+                                        title="Delete Research" {{-- onclick="return confirm(&quot;Confirm delete?&quot;)"
+                                --}}><i class="fa fa-trash" aria-hidden="true"></i></button>
+                            </form>
+                        </td>
+                    </tr>
                 @endforeach
-            </tbody>
-        </table>
-        <div class="d-flex justify-content-center">
-            {!! $products->links() !!}
-        </div>
+                </tbody>
+            </table>
+            {{--                                    <div class="d-flex justify-content-product">--}}
+            {{--                                        {!! $products->links() !!}--}}
+            {{--                                    </div>--}}
+                        </div>
+                    </div>
+                </div>
+
+
+{{--        <div class="d-flex justify-content-center">--}}
+{{--            {!! $products->links() !!}--}}
+{{--        </div>--}}
     </div>
 </div>
 @endsection
+
+
 @push('scripts')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $("#myInput").on("keyup", function() {
+                var value = $(this).val().toLowerCase();
+                $("#products tr").filter(function() {
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                });
+            });
+        });
+    </script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 
 <script type="text/javascript">
@@ -90,5 +119,23 @@
             });
         });
 </script>
+@endpush
+@push('styles')
+    <style>
+    table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+    }
 
+    td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+    }
+
+    tr:nth-child(even) {
+    background-color: #dddddd;
+    }
+    </style>
 @endpush
