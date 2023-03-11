d28d2c86fbce498d9ed1af56b7e4b26809869318
Nahin <nahin.ahmed28@gmail.com>
2023-03-11 23:59:59 +0600

    slider control color change

--- resources/views/frontend/layouts/common/hero.blade.php
+++ resources/views/frontend/layouts/common/hero.blade.php
@@ -9,11 +9,11 @@
     </div>
 
     <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="carousel-control-prev-icon" style=" filter: invert(100%);" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
     </a>
     <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="carousel-control-next-icon" style=" filter: invert(100%);" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
     </a>
 </div>
